dbf3f2fc4dc4d573f1bafe160bf8be3747490d37
bob4321at <judeberin113@gmail.com>
2025-10-23 16:02:28 -0600

    pls

--- ui/flake.nix
+++ ui/flake.nix
@@ -1,15 +1,34 @@
 {
-  description = "A very basic flake";
+  description = "bind_player flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "bind_player";
+        version = "1.0.0";
+        src = ./.;
+        vendorHash = null;
+        doCheck = false;
+        subPackages = [ "." ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+        buildInputs = [
+          pkgs.pkgconf
+          pkgs.gcc
+          pkgs.xorg.libX11
+          pkgs.xorg.libXrandr
+          pkgs.xorg.libXinerama
+          pkgs.xorg.libXcursor
+          pkgs.xorg.libXi
+          pkgs.mesa
+          pkgs.libGL
+          pkgs.xorg.libXxf86vm 
+        ];
+      };
+    };
 }
+
